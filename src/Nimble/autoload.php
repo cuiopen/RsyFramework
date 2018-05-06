1989da8d04093a7a43e146606a82fdf6086410fe
dreamans <dreamans@163.com>
2018-05-06 10:15:39 +0800

    增加autoload入口

---
+++ src/Nimble/autoload.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the RsyFramework package
+ *
+ * (c) Dreamans <dreamans@rsycoder.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__ . '/Autoload/Autoloader.php';
+
+return \Nimble\Autoload\Autoloader::initLoader();
